6fe62e663a1839a866a8f8237fd9ae4197370fcc
eloisa <maria-eloisa-santos@outlook.com>
2025-04-21 21:36:03 -0300

    feat: consulta contas não pagas #14

---
+++ database/tarefas/p01/projeto01-EL0ISA-q01.sql
@@ -0,0 +1,17 @@
+-- Consulta: As contas que vencer no mes atual que ainda não foram pagas ou que estão atrasadas
+SELECT cr.codigo, cr.aluno_cpf, cr.valor, cr.vencimento
+FROM conta_receber cr
+LEFT JOIN pagamento p ON cr.codigo = p.conta_receber_codigo AND p.status = TRUE
+WHERE 
+  (
+    -- Contas a vencer no mês atual e não pagas
+    (cr.vencimento >= date_trunc('month', current_date) 
+     AND cr.vencimento < date_trunc('month', current_date) + interval '1 month')
+    AND p.conta_receber_codigo IS NULL
+  )
+  OR
+  (
+    -- Contas atrasadas e não pagas
+    (cr.vencimento < current_date)
+    AND p.conta_receber_codigo IS NULL
+  );
