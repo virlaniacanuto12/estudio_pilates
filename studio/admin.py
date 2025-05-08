db505d241e55e6bdeee033ea72efa58a0f1b5921
virlaniacanuto12 <micarlacanuto12@gmail.com>
2025-05-08 10:47:47 -0300

    Adicionando create de Funcionario #31

--- studio/admin.py
+++ studio/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Servico # Importa o nosso modelo Servico
+from .models import Funcionario
+
 
 # Register your models here.
 
 # Registra o modelo Servico no site administrativo do Django
 admin.site.register(Servico)
+#admin.site.register(Funcionario)
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'funcao', 'salario', 'is_admin')  
+    search_fields = ('nome', 'cpf', 'funcao')  
+    list_filter = ('funcao', 'is_admin')  
