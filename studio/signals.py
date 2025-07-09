5c6556a1188f36b00710525eb297c85899c3e099
BeatrizVCosta <beatrizcostavi@gmail.com>
2025-07-09 14:53:54 -0300

    fix: Corrigi banco e bugs em agendamento/aula

--- studio/signals.py
+++ studio/signals.py
@@ -4,12 +4,18 @@ from .models import Agendamento, Aula, AulaAluno
 
 @receiver(post_save, sender=Agendamento)
 def criar_participacao(sender, instance, created, **kwargs):
-    aula = Aula.objects.filter(data=instance.data, horario=instance.horario).first()
+    aula = Aula.objects.filter(
+        data=instance.horario_disponivel.data,
+        horario=instance.horario_disponivel.horario_inicio
+    ).first()
     if aula:
         AulaAluno.objects.get_or_create(aula=aula, aluno=instance.aluno)
 
 @receiver(post_delete, sender=Agendamento)
 def remover_participacao(sender, instance, **kwargs):
-    aula = Aula.objects.filter(data=instance.data, horario=instance.horario).first()
+    aula = Aula.objects.filter(
+        data=instance.horario_disponivel.data,
+        horario=instance.horario_disponivel.horario_inicio
+    ).first()
     if aula:
         AulaAluno.objects.filter(aula=aula, aluno=instance.aluno).delete()
